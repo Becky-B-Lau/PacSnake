e3e95553fae39974cb802202cda50ca7529b9f26
Fannie A.S. Holm-Jensen <fah001@edu.zealand.dk>
2025-04-01 12:40:41 +0200

    pacman
    Movemten WASD

--- Assets/_Scripts/PacManMovement.cs
+++ Assets/_Scripts/PacManMovement.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class PacManMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [field: SerializeField]
+    public float speed { get; set; } = 12.0f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Update()
     {
-        
+        Vector2 move = new Vector2();
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.y += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.y -= speed;
+        }
+
+        rb.MovePosition(rb.position + (move * Time.deltaTime));
     }
 }

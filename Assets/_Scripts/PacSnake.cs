a0fd66347394076c983248c05805059a5f996384
Fannie A.S. Holm-Jensen <fah001@edu.zealand.dk>
2025-04-08 13:41:58 +0200

    .

--- Assets/_Scripts/PacSnake.cs
+++ Assets/_Scripts/PacSnake.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class GhostMovement : MonoBehaviour
+public class PacMan : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
